4e7799dbee8ffac73bbcbfda7c0625ffa6cba3a2
Vishal <vishal.prajapati@crestskillserve.com>
2026-07-10 12:41:51 +0530

    Add Laravel 9 support (composer constraints + replaceMigration fix)

--- src/Console/Command/CreateQueueCountTableMigration.php
+++ src/Console/Command/CreateQueueCountTableMigration.php
@@ -2,87 +2,37 @@
 
 namespace Garbetjie\Laravel\DatabaseQueue\Console\Command;
 
-use Illuminate\Console\Command;
-use Illuminate\Database\Migrations\MigrationCreator;
-use Illuminate\Filesystem\Filesystem;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Queue\Console\TableCommand;
-use Illuminate\Support\Composer;
 use Illuminate\Support\Str;
-use function pathinfo;
-use function str_replace;
-use const PATHINFO_FILENAME;
 
-class CreateQueueCountTableMigration extends Command
+class CreateQueueTableMigration extends TableCommand
 {
-    /**
-     * @var string
-     */
-    protected $signature = 'garbetjie:database-queue:table-job-counts';
+    protected $signature = 'garbetjie:database-queue:table';
 
-    /**
-     * @var string
-     */
-    protected $description = 'Create the migrations for storing queue counts in a separate table, and keeping them synchronised.';
+    protected $description = 'Create the migration for enabling optimistic locking on the queue table.';
 
-    /**
-     * @var Filesystem
-     */
-    protected $files;
-
-    /**
-     * @var Composer
-     */
-    protected $composer;
-
-    /**
-     * @param Filesystem $files
-     * @param Composer $composer
-     */
-    public function __construct(Filesystem $files, Composer $composer)
+    protected function createBaseMigration($table = 'jobs')
     {
-        parent::__construct();
-
-        $this->files = $files;
-        $this->composer = $composer;
+        return $this->laravel['migration.creator']->create(
+            'enable_optimistic_locking_on_'.$table.'_table',
+            $this->laravel->databasePath().'/migrations'
+        );
     }
 
-    public function handle()
+    /**
+     * @throws FileNotFoundException
+     */
+    protected function replaceMigration($path, $table)
     {
-        // Table migration.
-        $this->replaceMigration(
-            'create_database_queue_counts_01_table',
-            __DIR__ . '/queue_counts_table.stub'
-        );
+        $tableClassName = Str::studly($table);
 
-        // Trigger creation.
-        $this->replaceMigration(
-            'create_database_queue_counts_02_trigger',
-            __DIR__ . '/queue_counts_trigger.stub'
-        );
-
-        // Populate the counts table.
-        $this->replaceMigration(
-            'create_database_queue_counts_03_insertion',
-            __DIR__ . '/queue_counts_insert.stub'
-        );
-
-        $this->composer->dumpAutoloads();
-    }
-
-    protected function replaceMigration($migrationName, $stubPath)
-    {
-        $jobsTable = $this->laravel['config']['queue.connections.database.table'];
-        $table = $jobsTable . '_count';
-
-
-        $path = $this->laravel['migration.creator']->create($migrationName, $this->laravel->databasePath() . '/migrations');
         $stub = str_replace(
-            ['{{table}}', '{{className}}', '{{jobsTable}}'],
-            [$table, Str::studly($migrationName), $jobsTable],
-            $this->files->get($stubPath)
+            ['{{table}}', '{{tableClassName}}'],
+            [$table, $tableClassName],
+            $this->files->get(__DIR__.'/version.stub')
         );
 
         $this->files->put($path, $stub);
-        $this->info('Created migration ' . pathinfo($path, PATHINFO_FILENAME) . ' successfully.');
     }
 }
